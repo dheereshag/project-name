7254ce8962387b78c7a18e14aec68feee313b7bc
dheereshag <da11@iitbbs.ac.in>
2026-04-05 21:30:14 +0530

    feat: add health check endpoint and corresponding tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -18,5 +18,9 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toBe('Hello World!');
     });
+
+    it('should return API health', () => {
+      expect(appController.getHealth()).toEqual({ status: 'ok' });
+    });
   });
 });
